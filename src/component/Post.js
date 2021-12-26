3b1dac411fd05f685d1068ad93b946e437ea4e6d
Gabriel <gabrielcpf2997@gmail.com>
2021-12-26 14:48:58 -0800

    post.js commit

--- src/component/Post.js
+++ src/component/Post.js
@@ -1,7 +1,6 @@
 import { DateTime } from "luxon";
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
-import async from "async";
 
 const Post = () => {
     const id = useParams().id;
