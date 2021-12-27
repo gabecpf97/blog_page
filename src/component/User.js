224cbcc7907b3480699367103ea92b2fc0c2736e
Gabriel <gabrielcpf2997@gmail.com>
2021-12-26 19:36:50 -0800

    changing user.js

--- src/component/User.js
+++ src/component/User.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const User = () => {
+    const id = useParams().id;
+    const [posts, setPosts] = useState();
+    const [comments, setComments] = useState();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(`http://localhost:5000/user/${id}/post_comment`);
+            const data = await response.json();
+            console.log(data);
+        }
+        fetchData();
+    }, [id]);
+
 
     return (
         <div className="user_detail">
-            
+
         </div>
     )
 }
